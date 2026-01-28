06979f0c7d90e1cc6ef080f332670da9f6625c51
Abdullah Mohammad <abdullah2207019@gmail.com>
2026-01-28 23:25:30 +0200

    fix: removed some apis

--- lib/features/view/upload_screen.dart
+++ lib/features/view/upload_screen.dart
@@ -4,7 +4,6 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:image_picker/image_picker.dart';
-import '../../core/service/service_locator.dart';
 import '../viewmodel/prediction_cubit.dart';
 import '../viewmodel/prediction_state.dart';
 
